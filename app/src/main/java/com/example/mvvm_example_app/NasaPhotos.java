2a505694597727e988dc5f028e33946f5f370ff9
ejanik1997 <235297@student.pwr.edu.pl>
2021-10-19 23:52:57 +0200

    Fixing security issues

--- app/src/main/java/com/example/mvvm_example_app/NasaPhotos.java
+++ app/src/main/java/com/example/mvvm_example_app/NasaPhotos.java
@@ -1,16 +1,29 @@
 package com.example.mvvm_example_app;
 
+
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 public class NasaPhotos {
+    private static final String TAG = "NasaPhotos";
     private String image;
     private String longitude;
     private String latitude;
 
     public NasaPhotos() {}
 
-    public NasaPhotos(String longitude, String latitude) {
+    public NasaPhotos(String longitude, String latitude, String key){
+        //String key = getApiKey();
         this.longitude = longitude;
         this.latitude = latitude;
-        this.image = "https://api.nasa.gov/planetary/earth/imagery?lon=-"+longitude+"&lat="+latitude+"&date=2018-01-01&dim=0.15&api_key=ARrHj7iS8nSPDazyFPamTbU1N0rmQsrQyggRUSrg";
+        this.image = "https://api.nasa.gov/planetary/earth/imagery?lon=-"+longitude+"&lat="+latitude+"&date=2018-01-01&dim=0.15&api_key=" + key;
 
     }
 
